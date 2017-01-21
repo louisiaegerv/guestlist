5e4f26874b3d3657617e3b46947a25e539b50347
louisiaegerv <louis.iaeger@gmail.com>
2017-01-20 21:49:33 -0600

    add filter functionality

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const form = document.querySelector('#registrar');
 const input = document.querySelector('#registrar input');
 const inviteButton = document.querySelector('#registrar button');
+const mainDiv = document.querySelector('.main');
 const ul = document.querySelector('#invitedList');
+
+const filterDiv = document.createElement('div');
+const filterLabel = document.createElement('label');
+const filterCheckbox = document.createElement('input');
+
+filterLabel.textContent = 'Hide those who haven\'t responded';
+filterCheckbox.classList.add('filterCheckbox');
+filterCheckbox.type = 'checkbox';
+filterDiv.appendChild(filterLabel);
+filterDiv.appendChild(filterCheckbox);
+mainDiv.insertBefore(filterDiv, ul);
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     if (input.value != '') {
@@ -36,13 +49,13 @@ function createLI(name) {
     li.appendChild(removeButton);
     ul.appendChild(li);
 }
-ul.addEventListener('change', function (e) {
+ul.addEventListener('change', (e) => {
     if (e.target.type == 'checkbox') {
         //dom traversal from checkbox > label > li
         e.target.parentNode.parentNode.classList.toggle('responded');
     }
 });
-ul.addEventListener('click', function (e) {
+ul.addEventListener('click', (e) => {
     if (e.target.tagName === 'BUTTON') {
         const button = event.target;
         const li = button.parentNode;
@@ -65,6 +78,26 @@ ul.addEventListener('click', function (e) {
             li.removeChild(input);
         } else if (button.textContent == 'remove') {
             ul.removeChild(li);
+        }
+    }
+});
+
+filterCheckbox.addEventListener('change', (e) => {
+    const isChecked = e.target.checked;
+    let lis = ul.children;
+    if (isChecked) {  
+        for (let i = 0; i < lis.length; i++) {
+            let li = lis[i];
+            if (li.className === 'responded') {
+                li.style.display = '';
+            } else {
+                li.style.display = 'none';
+            }
+        }
+    } else {  
+        for (let i = 0; i < lis.length; i++) {
+            let li = lis[i];
+            li.style.display = '';
         }
     }
 });
