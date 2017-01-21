3f600309bb8b344937d4dc22b76f4df0bf663c91
louisiaegerv <louis.iaeger@gmail.com>
2017-01-21 17:34:28 -0600

    Add localstorage functinoality

--- app.js
+++ app.js
@@ -3,105 +3,152 @@ const input = document.querySelector('#registrar input');
 const inviteButton = document.querySelector('#registrar button');
 const mainDiv = document.querySelector('.main');
 const ul = document.querySelector('#invitedList');
-
 const filterDiv = document.createElement('div');
 const filterLabel = document.createElement('label');
 const filterCheckbox = document.createElement('input');
-
-filterLabel.textContent = 'Hide those who haven\'t responded';
+filterLabel.textContent = 'Hide those who haven\'t been confirmed';
 filterCheckbox.classList.add('filterCheckbox');
 filterCheckbox.type = 'checkbox';
 filterLabel.appendChild(filterCheckbox);
 filterDiv.appendChild(filterLabel);
 mainDiv.insertBefore(filterDiv, ul);
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    if (input.value != '') {
-        let personName = input.value;
-        //guests.push(personName);
-        input.value = '';
-        input.focus();
-        createLI(personName);
+// ---- Functions ---- //
+const supportsLocalStorage = () => {
+    try {
+        return 'localStorage' in window && window['localstorage'] !== null;
     }
-});
-
-function createLI(name) {
-    const li = document.createElement('li');
-//    li.textContent = name;
-    const span = document.createElement('span');
-    span.textContent = name;
-    const label = document.createElement('label');
-    label.textContent = 'Confirmed';
-    const checkBox = document.createElement('input');
-    checkBox.type = 'checkbox';
-    const editButton = document.createElement('button');
-    editButton.classList.add('editButton');
-    editButton.textContent = 'edit';
-    const removeButton = document.createElement('button');
-    removeButton.classList.add('removeButton');
-    removeButton.textContent = 'remove';
-    
-    li.appendChild(span);
-    label.appendChild(checkBox);
-    li.appendChild(label);
-    li.appendChild(editButton);
-    li.appendChild(removeButton);
-    ul.appendChild(li);
+    catch (e) {
+        return false;
+    }
+};
+const getLocalStorage = () => {
+    let guests = localStorage.getItem('guests');
+    //console.log(guests);
+    if (guests) {
+        return JSON.parse(guests);
+    }
+    return [];
 }
-ul.addEventListener('change', (e) => {
-    if (e.target.type == 'checkbox') {
-        //dom traversal from checkbox > label > li
-        e.target.parentNode.parentNode.classList.toggle('responded');
+const saveToLocalStorage = (guestName) => {
+    let guests = getLocalStorage();
+    if (!guestName && guests.indexOf(guestName) > -1) {
+        return false;
     }
-});
-ul.addEventListener('click', (e) => {
-    if (e.target.tagName === 'BUTTON') {
-        const button = event.target;
-        const li = button.parentNode;
-        const ul = li.parentNode;
-        if (button.textContent == 'edit') {
-            button.textContent = 'save';
-            const span = li.firstElementChild;
-            const input = document.createElement('input');
-            input.type = 'text';
-            input.value = span.textContent;
-            li.insertBefore(input, span);
-            li.removeChild(span);
-            input.focus();
-        } else if (button.textContent === 'save') {
-            button.textContent = 'edit';
-            const input = li.firstElementChild;
-            const span = document.createElement('span');
-            span.textContent = input.value;
-            li.insertBefore(span, input);
-            li.removeChild(input);
-        } else if (button.textContent == 'remove') {
-            ul.removeChild(li);
-        }
-    }
-});
+    guests.push(guestName);
+    localStorage.setItem('guests', JSON.stringify(guests));
+    return true;
+}
+const removeLocalStorageList = () => {
+    localStorage.removeItem('guests');
+}
+const removeGuestFromLocalStorage = (guestName) => {
+    
+}
 
-filterCheckbox.addEventListener('change', (e) => {
-    const isChecked = e.target.checked;
-    let lis = ul.children;
-    if (isChecked) {  
-        for (let i = 0; i < lis.length; i++) {
-            let li = lis[i];
-            if (li.className === 'responded') {
-                li.style.display = '';
-            } else {
-                li.style.display = 'none';
+const createLI = (name) => {
+    const createElement = (elementName, property, value) => {
+        const element = document.createElement(elementName);
+        element[property] = value;
+        return element;
+    };
+    const appendToLI = (elementName, property, value) => {
+        const element = createElement(elementName, property, value);
+        li.appendChild(element);
+        return element;
+    };
+    const li = document.createElement('li');
+    ul.appendChild(li);
+    appendToLI('span', 'textContent', name);
+    appendToLI('label', 'textContent', 'Confirmed').appendChild(createElement('input', 'type', 'checkbox'));
+    appendToLI('button', 'textContent', 'edit');
+    appendToLI('button', 'textContent', 'remove');
+};
+window.onload = () => {
+    // ---- Event Listeners ---- //
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        if (input.value != '') {
+            let personName = input.value;
+            input.value = '';
+            input.focus();
+            saveToLocalStorage(personName);
+            createLI(personName);
+        }
+    });
+    ul.addEventListener('change', (e) => {
+        if (e.target.type == 'checkbox') {
+            //dom traversal from checkbox > label > li
+            e.target.parentNode.parentNode.classList.toggle('responded');
+        }
+    });
+    ul.addEventListener('click', (e) => {
+        if (e.target.tagName === 'BUTTON') {
+            const button = event.target;
+            const li = button.parentNode;
+            const ul = li.parentNode;
+            const action = button.textContent;
+            const guests = getLocalStorage();
+            //console.log(guests);
+            
+            const nameActions = {
+                    remove: () => {
+                        let name = li.firstElementChild.textContent;
+                        console.log(name + " has been removed.");
+                        let guests = getLocalStorage();
+                        let guestIndex = guests.indexOf(name);
+                        console.log(guestIndex);
+                        let removedGuest = guests.splice(guestIndex, 1);
+                        console.log(removedGuest);
+                        localStorage.setItem('guests', JSON.stringify(guests));
+                        ul.removeChild(li);
+                    }
+                    , edit: () => {
+                        button.textContent = 'save';
+                        const span = li.firstElementChild;
+                        const input = document.createElement('input');
+                        input.type = 'text';
+                        input.value = span.textContent;
+                        li.insertBefore(input, span);
+                        li.removeChild(span);
+                        input.focus();
+                    }
+                    , save: () => {
+                        button.textContent = 'edit';
+                        const input = li.firstElementChild;
+                        const span = document.createElement('span');
+                        span.textContent = input.value;
+                        li.insertBefore(span, input);
+                        li.removeChild(input);
+                    }
+                }
+                //select and run action in buttons name
+            nameActions[action]();
+        }
+    });
+    filterCheckbox.addEventListener('change', (e) => {
+        const isChecked = e.target.checked;
+        let lis = ul.children;
+        if (isChecked) {
+            for (let i = 0; i < lis.length; i++) {
+                let li = lis[i];
+                if (li.className === 'responded') {
+                    li.style.display = '';
+                }
+                else {
+                    li.style.display = 'none';
+                }
             }
         }
-    } else {  
-        for (let i = 0; i < lis.length; i++) {
-            let li = lis[i];
-            li.style.display = '';
+        else {
+            for (let i = 0; i < lis.length; i++) {
+                let li = lis[i];
+                li.style.display = '';
+            }
         }
-    }
-});
-
-guests.forEach((guest) => {
-   createLI(guest); 
-});
+    });
+    
+    //Get localstorage guests and display at start
+    getLocalStorage().forEach((guest) => {
+        createLI(guest);
+    });
+};
